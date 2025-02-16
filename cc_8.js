f0014f507401120122e50917ad915abec00ab73c
Tyler Paiewonsky <tylerpsky@gmail.com>
2025-02-16 14:01:12 -0500

    Task 2 - Product Price After Discount.

--- cc_8.js
+++ cc_8.js
@@ -8,3 +8,13 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 console.log(calculateSalary(5000, 500, 0.1)); 
 console.log(calculateSalary(7000, 1000, 0.15)); 
 
+//Task 2
+
+const calculateDiscount = function (price, discountRate) {
+    let finalPrice = price - (price * discountRate);
+    return `Final Price: $${finalPrice.toFixed(2)}`;
+}
+
+console.log(calculateDiscount(100, 0.2));
+console.log(calculateDiscount(250, 0.15));
+
