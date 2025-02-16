42e72b481e38b861e661138ee15195d509105012
Tyler Paiewonsky <tylerpsky@gmail.com>
2025-02-16 13:58:04 -0500

    Task 1 - Employee Salary Calculation

---
+++ cc_8.js
@@ -0,0 +1,10 @@
+//Task 1
+
+function calculateSalary(baseSalary, bonus, taxRate) {
+    let netSalary = (baseSalary + bonus) - (baseSalary * taxRate);
+    return `Net Salary: $${netSalary.toFixed(2)}`;
+}
+
+console.log(calculateSalary(5000, 500, 0.1)); 
+console.log(calculateSalary(7000, 1000, 0.15)); 
+
